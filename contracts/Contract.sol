9d07c5907ee1f62d916be7df82fb7362cfa58887
Virendra Khorwal <virendrakhorwalvk@gmail.com>
2023-09-28 23:39:09 +0530

    thirdWeb learning

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -1,6 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.13;
 
-contract MyContract {
-    constructor() {}
+contract HelloWorld {
+
+    string public message;
+
+    constructor() {
+        message="Hello World";
+    }
+
+    function getMessage() public view returns (string memory) {
+        return message;
+    }
 }
